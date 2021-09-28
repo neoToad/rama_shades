a19d44246624b861325696053e19b22cf01b6ac1
neoToad <colinjmass@gmail.com>
2021-09-28 14:55:55 -0400

    First

--- store/views.py
+++ store/views.py
@@ -439,6 +439,39 @@ def remove_single_item_from_cart(request, slug):
 
 
 def home(request):
+    if request.user.is_authenticated:
+        order, created = Order.objects.get_or_create(user=request.user, ordered=False)
+        cart_items = order.get_cart_items
+    else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except KeyError:
+            cart = {}
+        print('Cart:', cart)
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cart_items = order['get_cart_items']
+
+        for i in cart:
+            cart_items += cart[i]['quantity']
+
+            product = Item.objects.get(id=i)
+            total = (product.price * cart[i]['quantity'])
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]['quantity']
+
+            item = {
+                'product': {
+                    'id': product.id,
+                    'title': product.title,
+                    'price': product.price,
+                },
+                'quantity': cart[i]['quantity'],
+                'get_total': total,
+            }
+            items.append(item)
+
     items = Item.objects.all()
     context = {'object_list': items, }
     return render(request, 'store/home.html', context)
