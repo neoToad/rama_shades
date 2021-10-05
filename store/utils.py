01f132e492edb3e12250b77b1fa62fb85449e672
neoToad <colinjmass@gmail.com>
2021-10-05 14:22:09 -0400

    Back to styles - guest order summary - remove items now

--- store/utils.py
+++ store/utils.py
@@ -13,36 +13,39 @@ def cookie_cart(request):
     cart_items = order['get_cart_items']
 
     for i in cart:
-        try:
-            cart_items += cart[i]['quantity']
+        # try:
+        cart_items += cart[i]['quantity']
 
-            product = Item.objects.get(id=i)
-            total = (product.discount_price * cart[i]['quantity'])
+        product = Item.objects.get(id=i)
+        total = (product.discount_price * cart[i]['quantity'])
 
-            order['get_cart_total'] += total
-            order['get_cart_items'] += cart[i]['quantity']
+        order['get_cart_total'] += total
+        order['get_cart_items'] += cart[i]['quantity']
 
-            order['get_shipping'] = Decimal(7 + (cart[i]['quantity'] * .5))
+        order['get_shipping'] = Decimal(7 + (cart[i]['quantity'] * .5))
 
-            order['total'] = order['get_cart_total'] + order['get_shipping']
+        order['total'] = order['get_cart_total'] + order['get_shipping']
 
-            get_amount_saved = product.price - product.discount_price
+        get_amount_saved = product.price - product.discount_price
 
-            item = {
-                'product': {
-                    'id': product.id,
-                    'title': product.title,
-                    # 'style': cart[i]['style'],
-                    'price': product.price,
-                    'discount_price': product.discount_price,
-                },
-                'quantity': cart[i]['quantity'],
-                'get_total': total,
-                'get_amount_saved': get_amount_saved
-            }
-            items.append(item)
-        except:
-            cart_items -= cart[i]['quantity']
+        for style in cart[i]:
+            if style != 'quantity':
+
+                item = {
+                    'product': {
+                        'id': product.id,
+                        'title': product.title,
+                        'style': style,
+                        'price': product.price,
+                        'discount_price': product.discount_price,
+                    },
+                    'quantity': cart[i][style]['quantity'],
+                    'get_total': total,
+                    'get_amount_saved': get_amount_saved
+                }
+                items.append(item)
+        # except:
+        #     cart_items -= cart[i]['quantity']
     return {'items': items, 'cart_items': cart_items, 'object': order,
             'categories': ['Square', 'Cat Eye', 'Round', 'Rectangular',
                   'Oval', 'Unique']}
