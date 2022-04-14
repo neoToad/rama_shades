b909639df4f8404c36e7d7f2cadbadcfcff2f594
neoToad <82552068+neoToad@users.noreply.github.com>
2022-04-14 11:23:44 -0400

    Update settings.py

--- rama_shades/settings.py
+++ rama_shades/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-47ykas)b+)or^j!x84iy)!&o)#fu%6$_r2-j60k-yt!2nb=4%q'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
